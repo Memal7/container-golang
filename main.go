354220fa7245f2a1eae29411fc46035a8e3bdf11
Mustafa Emal <mustafa.emal@microsoft.com>
2023-02-12 15:55:44 +0100

    child func added

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
+// import the necessary packages
 import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
+// main function
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("what?")
 	}
 }
 
+// run function - create a new process
 func run() {
+	fmt.Println("running \n", os.Args[2:])
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+// create a new UTS namespace for the process (set hostname, domainname, etc.)
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	must(cmd.Run())
+}
+
+// child function - set hostname
+func child() {
 	fmt.Println("running \n", os.Args[2:])
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
@@ -23,6 +45,8 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	must(syscall.Sethostname([]byte("container")))
+	
 	must(cmd.Run())
 
 }
